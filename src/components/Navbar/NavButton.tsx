2e2beb56b5560b56b6b0b93f5831f1819ba4d480
JoshAlexis <joshyelc@gmail.com>
2022-01-11 19:39:46 -0700

    Navbar components

---
+++ src/components/Navbar/NavButton.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+type NavButtonProps = {
+  title: string,
+  to: string
+}
+
+const NavButton = ({ title, to }: NavButtonProps) => {
+  return (
+    <Link
+      to={to}
+      className="md:w-28 text-center px-2 py-1 transform transition duration-200 ease-out
+    text-gray-100 font-semibold text-lg"
+    >
+      {title}
+    </Link>
+  );
+};
+
+export default NavButton;
