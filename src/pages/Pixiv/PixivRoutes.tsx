f5740ea8e9fd4e9fb90f3a82026c1672307fc08a
JoshAlexis <joshyelc@gmail.com>
2022-01-27 20:17:03 -0700

    Route for AddPixiv

--- src/pages/Pixiv/PixivRoutes.tsx
+++ src/pages/Pixiv/PixivRoutes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import AddPixiv from './AddPixiv';
 import PixivHome from './PixivHome';
 
 const PixivRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<PixivHome />} />
+      <Route path="/add" element={<AddPixiv />} />
     </Routes>
   );
 };
