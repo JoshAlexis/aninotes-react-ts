f13ef38212e25a92801a57697c66a9fe181ba0e0
JoshAlexis <joshyelc@gmail.com>
2022-02-06 11:13:06 -0700

    Change export instance

--- src/services/ApiProvider.ts
+++ src/services/ApiProvider.ts
@@ -1,19 +1,7 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
-class ApiProvider {
-  constructor(private api: AxiosInstance) {
-    this.api = api;
-  }
-
-  get(url: string, config: AxiosRequestConfig) {
-    return this.api.get(url, config);
-  }
-
-  add(url: string, config: AxiosRequestConfig) {
-    return this.api.post(url, config);
-  }
-}
-
-export default new ApiProvider(axios.create({
+const ApiProvider: AxiosInstance = axios.create({
   baseURL: 'https://aninotesapi.herokuapp.com/api/',
-}));
+});
+
+export default ApiProvider;
